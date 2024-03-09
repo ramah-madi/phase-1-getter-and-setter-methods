72cdbac24dd873c85078c1c06b4068b2639ba0fb
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2024-03-09 21:21:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+
+  // Getter method for diameter
+  get diameter() {
+    return this.radius * 2;
+  }
+
+  // Setter method for diameter
+  set diameter(value) {
+    this.radius = value / 2;
+  }
+
+  // Getter method for circumference
+  get circumference() {
+    return 2 * Math.PI * this.radius;
+  }
+
+  // Setter method for circumference
+  set circumference(value) {
+    this.radius = value / (2 * Math.PI);
+  }
+
+  // Getter method for area
+  get area() {
+    return Math.PI * this.radius ** 2;
+  }
+
+  // Setter method for area
+  set area(value) {
+    this.radius = Math.sqrt(value / Math.PI);
+  }
+}
